62bc9b4a228182f76d024b47ac6e475d8156bc41
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-20 23:55:59 +0330

    [create]-basic comment style
    Description:
    create basic comment style structure

--- Interactive comments/script.js
+++ Interactive comments/script.js
@@ -1,5 +1,21 @@
 let form = document.getElementById("form");
-
+let text = document.getElementById("textA");
+let btn = document.getElementById("btn");
+let ul = document.getElementById("ul");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+
+btn.addEventListener("click", clickButton);
+
+function clickButton() {
+  console.log('click');
+  let li = document.createElement("li");
+  let edit = document.createElement("span");
+  let user = text.value;
+  li.innerHTML = user;
+  edit.innerHTML = '🖋️'
+  li.append(edit)
+  li.setAttribute('autocomplete' , 'on')
+  ul.append(li)
+}
