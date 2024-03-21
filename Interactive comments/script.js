61fba3598d3c14eebdcc97996998a9d3ba065f68
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-21 04:42:08 +0330

    [add]-delete and edit button
    Description:
    add delete and edit button

--- Interactive comments/script.js
+++ Interactive comments/script.js
@@ -9,13 +9,24 @@ form.addEventListener("submit", (e) => {
 btn.addEventListener("click", clickButton);
 
 function clickButton() {
-  console.log('click');
+  console.log("click");
   let li = document.createElement("li");
   let edit = document.createElement("span");
+  let delet = document.createElement("span");
+  let p = document.createElement("p");
   let user = text.value;
-  li.innerHTML = user;
-  edit.innerHTML = '🖋️'
-  li.append(edit)
-  li.setAttribute('autocomplete' , 'on')
-  ul.append(li)
+  p.innerHTML = user;
+  edit.innerHTML = "🖋️";
+  delet.innerHTML += "🗑️";
+  li.append(edit);
+  li.append(p);
+  li.append(delet);
+  li.setAttribute("autocomplete", "on");
+  ul.append(li);
+  edit.addEventListener("click", () => {
+    p.contentEditable = true;
+  });
+  delet.addEventListener("click", () => {
+    li.style.display = "none";
+  });
 }
