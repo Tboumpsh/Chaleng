8a54f224266e00e6de6a7177bf71bd4c141c332a
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-21 04:46:03 +0330

    [add]-textarea error
    Description:
    textArea cant is null

--- Interactive comments/script.js
+++ Interactive comments/script.js
@@ -9,24 +9,28 @@ form.addEventListener("submit", (e) => {
 btn.addEventListener("click", clickButton);
 
 function clickButton() {
-  console.log("click");
-  let li = document.createElement("li");
-  let edit = document.createElement("span");
-  let delet = document.createElement("span");
-  let p = document.createElement("p");
-  let user = text.value;
-  p.innerHTML = user;
-  edit.innerHTML = "🖋️";
-  delet.innerHTML += "🗑️";
-  li.append(edit);
-  li.append(p);
-  li.append(delet);
-  li.setAttribute("autocomplete", "on");
-  ul.append(li);
-  edit.addEventListener("click", () => {
-    p.contentEditable = true;
-  });
-  delet.addEventListener("click", () => {
-    li.style.display = "none";
-  });
+  if (text.value == "" || text.value == null) {
+    alert("Write in the textArea");
+  } else {
+    console.log("click");
+    let li = document.createElement("li");
+    let edit = document.createElement("span");
+    let delet = document.createElement("span");
+    let p = document.createElement("p");
+    let user = text.value;
+    p.innerHTML = user;
+    edit.innerHTML = "🖋️";
+    delet.innerHTML += "🗑️";
+    li.append(edit);
+    li.append(p);
+    li.append(delet);
+    li.setAttribute("autocomplete", "on");
+    ul.append(li);
+    edit.addEventListener("click", () => {
+      p.contentEditable = true;
+    });
+    delet.addEventListener("click", () => {
+      li.style.display = "none";
+    });
+  }
 }
