1d891d65f3014a74d181b04fb16e42b98b1d5ebb
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-22 12:35:29 +0330

    [add]-slider value show
    Description:
    slider value show in html
    +innerhtml
    +oninput
    +value

--- Interactive pricing component/script.js
+++ Interactive pricing component/script.js
@@ -1,1 +1,13 @@
-let text = document.getElementById('text')
+// select
+
+let text = document.getElementById("text");
+let slider = document.getElementById("slider");
+
+// get slider value
+let range = slider.value;
+// slider value in text
+text.value = range;
+// update h1 value
+slider.oninput = function () {
+  text.innerHTML = `$ ${this.value}       <span>  /month </span>`;
+};
