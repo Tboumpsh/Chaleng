fa01b2a6e91ad2b2caf9b90fea2d0bf7ab557295
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-23 20:30:58 +0330

    [create]-base structure tags
    create base structure tags search

---
+++ Job listings with filtering/script.js
@@ -0,0 +1,8 @@
+let array = [
+  { name: "html", tags: ["developer", "Frontend", "CSS", "HTML"] },
+  { name: "JavaScript", tags: ["developer", "Frontend", "CSS", "cleanCode"] },
+  { name: "Photoshap", tags: ["design", "edit", "draw", "character"] },
+  { name: "C", tags: ["Junior", "backend", "cleanCode", "developer"] },
+  { name: "python", tags: ["backend", "devops", "Frontend", "developer"] },
+  { name: "Premier", tags: ["render", "edit", "design", "video"] },
+];
